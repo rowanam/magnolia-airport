3c000aee33156857f10c4876d264a0a4bbc561f7
rowanam <rowanamaas@gmail.com>
2023-07-24 22:47:18 +0000

    feat: add code to view flight passenger details

--- run.py
+++ run.py
@@ -11,3 +11,39 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('magnolia_airport')
+
+
+def readable_passenger_details(passenger):
+    """
+    Create a string with the details for the passed passenger, printed in a human-readable format
+    """
+    readable_details = f"{passenger['first name']} {passenger['last name']}"
+
+    # Add line for each detail except first and last name to string
+    for key, value in passenger.items():
+        if key == "first name" or key == "last name":
+            continue
+        
+        readable_details += (f"\n   {key.capitalize()}: {value}")
+
+    readable_details += f"\n"
+    
+    return readable_details
+
+
+def view_all_passenger_details(flight_number):
+    """
+    View all passengers and their details on the flight with the provided number
+    """
+    worksheet_to_view = SHEET.worksheet(flight_number)
+
+    # Get passenger info as a list of dictionaries
+    passengers = worksheet_to_view.get_all_records()
+
+    passengers_display_string = ""
+
+    for passenger in passengers:
+        details = readable_passenger_details(passenger)
+        passengers_display_string += f"{details}\n"
+    
+    return passengers_display_string
